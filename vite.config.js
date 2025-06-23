e0b54ac4a75d3da32b990625d854fef5c38c7b1d
jsause <johnsause331@gmail.com>
2025-06-23 15:19:00 +0800

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/React.js_Practice/'
-})
+  base: "/react-practice",
+});
 
